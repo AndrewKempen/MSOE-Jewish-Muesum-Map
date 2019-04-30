94e2dc6e2d2987220b72a6c4c070b66578d7c9a6
Andrew Kempen <Wishbonea@users.noreply.github.com>
2019-04-30 11:20:49 -0500

    Initial Commit

---
+++ openMap.bat
@@ -0,0 +1,7 @@
+title Open JMM WWII/faye's story map
+echo Open JMM WWII/faye's story map
+pause
+cd C:\Program Files (x86)\Google\Chrome\Application
+chrome.exe --disable-web-security --user-data-dir="[some directory here]" --disable-overscroll-history-navigation
+pause
+start chrome.exe .\test.html
